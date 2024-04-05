37354bd627cb556e418176c572013745e2d32fe2
亡灵暴龙大帝 <q200608233416@163.com>
2024-04-05 17:50:53 +0800

    update

--- HSR_Bytes-Tool.py
+++ HSR_Bytes-Tool.py
@@ -1,46 +1,54 @@
 TEXTS = [
-    "by亡灵暴龙大帝",#OverSeaLive
-    "com.HoYoverse.hkrpgoversea",#可以随意更改
-    "Star Rail",#可以随意更改
-    "使用半夏公益服公开文件制作",#OVERSEA_BUILD
-    "http://your_url.com:666" + "/query_dispatch"#修改前半部分内容为服务器地址
+    "by亡灵暴龙大帝",  # OverSeaLive Customizable 可以随意更改
+    "只因你太美",  # com.HoYoverse.hkrpgoversea Customizable 可以随意更改
+    "What can I say?",  # Star Rail Customizable 可以随意更改
+    "Mamba out",  # OVERSEA_BUILD Customizable 可以随意更改
+
+    # Modify the first half to the server address 修改前半部分内容为服务器地址
+    "http://your_url.com:666" + "/query_dispatch"
 ]
 
 def generate_file_with_length(texts):
-    # 准备用于存储整个文件内容的字节数组
+    # Prepare a byte array to store the entire file content 准备用于存储整个文件内容的字节数组
     file_content = bytearray()
 
-    # 添加文件开头的00间隔
+    # Add 00 separators at the beginning of the file 添加文件开头的00间隔
     file_content.extend(bytearray.fromhex("00"))
 
-    # 对于每个字符串
+    # For each string 对于每个字符串
     for index, text in enumerate(texts):
-        # 在每一组字符串前面添加一个十六进制的00间隔
         if index > 0:
             if index == 4:
-                file_content.extend(bytearray.fromhex("0000000200"))
+                # 对于第四个字符串，添加特定的字节表示两个IP地址
+                file_content.extend(bytearray.fromhex("0000000100"))
             else:
+                # 对于其他字符串，添加普通的00间隔
                 file_content.extend(bytearray.fromhex("00"))
 
+        # Calculate the number of bytes after UTF-8 encoding and convert it to hexadecimal
         # 计算UTF-8编码后的字节数量并转换为十六进制
         byte_count = len(text.encode("utf-8"))
         byte_count_hex = hex(byte_count)[2:].zfill(2)
 
+        # Add the hexadecimal representation of byte count as leading byte
         # 添加字节数量的十六进制表示作为前导字节
         file_content.append(int(byte_count_hex, 16))
 
-        # 构造数据内容
+        # Construct data content 构造数据内容
         content = bytearray()
         content.extend(text.encode("utf-8"))
 
-        # 将数据内容添加到文件内容中
+        # Append data content to file content 将数据内容添加到文件内容中
         file_content.extend(content)
 
-    # 写入文件
+    # Add 16 hexadecimal '00's at the end of the file 在文件末尾添加16个十六进制的'00'
+    file_content.extend(bytearray.fromhex("00" * 16))
+
+    # Write to file 写入文件
     with open("ClientConfig.bytes", "wb") as f:
         f.write(file_content)
 
-    print("文件生成成功。")
+    print("文件生成成功。success")
 
 if __name__ == "__main__":
     generate_file_with_length(TEXTS)
